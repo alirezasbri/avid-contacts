67e1403797ed3c687ecf12e610dbff6bd3c49a6c
alireza.sabouri <alireza.a.saburi@gmail.com>
2020-08-10 14:18:29 +0430

    create UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'family' => Str::random(10),
+            'username' => Str::random(8),
+            'password' => Hash::make('password'),
+            'type' => 'admin'
+        ]);
+    }
+}
