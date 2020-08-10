a90ed0358ea17ae7049dda5058a46dcf0624fdf9
alireza.sabouri <alireza.a.saburi@gmail.com>
2020-08-10 12:18:16 +0430

    edit show contact view

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -1,35 +1,55 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="fa">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Contacts</title>
 @include('layouts.bootstrap')
+<!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-
+</head>
 <body style="margin: 20px">
-<h1>{{$contact->name . ' ' . $contact->family}}</h1>
+<h1 align="right">{{$contact->name . ' ' . $contact->family}}</h1>
 
-<h2>PhoneNumbers</h2>
-<table class="table">
-    @foreach( $phoneNumbers as $phoneNumber)
-        <tr>
-            <td>{{$phoneNumber->phone_number}}</td>
-            <td>{{$phoneNumber->type}}</td>
-        </tr>
+<div align="right">
+    <h2>شماره تماس‌ها</h2>
+    <table class="table table-striped">
+        @foreach( $phoneNumbers as $phoneNumber)
+            <tr>
+                <td>{{$phoneNumber->phone_number}}</td>
 
-    @endforeach
-</table>
+                @switch($phoneNumber->type)
+                    @case('mobile')
+                    <td>موبایل</td>
+                    @break
+
+                    @case('phone')
+                    <td>ثابت</td>
+                    @break
+                @endswitch
+            </tr>
+
+        @endforeach
+    </table>
 
 
-<h2>Emails</h2>
-<table class="table">
-    @foreach( $emails as $email)
-        <tr>
-            <td>{{$email->email_address}}</td>
-        </tr>
-    @endforeach
-</table>
+    <h2>ایمیل‌ها</h2>
+    <table class="table">
+        @foreach( $emails as $email)
+            <tr>
+                <td>{{$email->email_address}}</td>
+            </tr>
+        @endforeach
+    </table>
 
-@if($editable)
-    <a href="{{route('contact.edit.form',[$contact->slug])}}">
-        {{--<a href="{{route('user.login.form')}}">--}}
-        <button class="btn btn-danger" style="margin: 5px">اصلاح مخاطب</button>
-    </a>
-@endif
-{{--<h1> {{ $contact->phone_number . ' ' . $contact->family }}</h1>--}}
+    @if($editable)
+        <a href="{{route('contact.edit.form',[$contact->slug])}}">
+            <button class="btn btn-danger" style="margin: 5px">اصلاح مخاطب</button>
+        </a>
+    @endif
+
+
+</div>
 </body>
