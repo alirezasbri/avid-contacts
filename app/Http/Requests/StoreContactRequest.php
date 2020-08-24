f176d54c5122922b9beb402bd1911e703753c4d5
alireza.sabouri <alireza.a.saburi@gmail.com>
2020-08-24 09:04:56 +0430

    delete a commnet line in StoreContactRequest

--- app/Http/Requests/StoreContactRequest.php
+++ app/Http/Requests/StoreContactRequest.php
@@ -30,7 +30,6 @@ class StoreContactRequest extends FormRequest
             'emails.*' => 'email:rfc,dns',
             'phones' => 'required|array|min:1',
             'phones.*.phone' => ['regex:/^(\+98|0098|98|0)[1-9]\d{9}$/'],
-//            'types' => 'required|array|min:1',
             'photo_name' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ];
     }
