4c8c554b6038594c61a8e54db1fa874def3cbc0d
alireza.sabouri <alireza.a.saburi@gmail.com>
2020-08-26 08:15:38 +0430

    edit helpers file

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -14,26 +14,26 @@ if (!function_exists('updateImage')) {
     {
         $contact->image()->update(['image' => prepareImage($files)]);
     }
+}
 
-    if (!function_exists('prepareImage')) {
-        function prepareImage($files): string
-        {
-            $destinationPath = 'public/image/'; // upload path
-            $imageName = date('YmdHis') . "." . $files->getClientOriginalExtension();
-            $files->move($destinationPath, $imageName);
-            return $imageName;
-        }
+if (!function_exists('prepareImage')) {
+    function prepareImage($files): string
+    {
+        $destinationPath = 'public/image/'; // upload path
+        $imageName = date('YmdHis') . "." . $files->getClientOriginalExtension();
+        $files->move($destinationPath, $imageName);
+        return $imageName;
     }
+}
 
-    if (!function_exists('handleImageInUpdateContact')) {
-        function handleImageInUpdateContact($files, $contact): void
-        {
-            if ($files) {
-                $contact->has('image') ? updateImage($files, $contact) : storeImage($files, $contact);
-            } else {
-                if ($contact->has('image'))
-                    $contact->image()->delete();
-            }
+if (!function_exists('handleImageInUpdateContact')) {
+    function handleImageInUpdateContact($files, $contact): void
+    {
+        if ($files) {
+            $contact->has('image') ? updateImage($files, $contact) : storeImage($files, $contact);
+        } else {
+            if ($contact->has('image'))
+                $contact->image()->delete();
         }
     }
 }
